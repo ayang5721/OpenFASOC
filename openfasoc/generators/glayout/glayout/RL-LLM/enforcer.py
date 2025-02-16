f85e08f2a37ffc93b6765c8cb9798ca6c660f487
ayang5721 <ayang5721@gmail.com>
2025-02-16 18:16:19 -0500

    2/16 - model fraimwork enforcer software side done

--- openfasoc/generators/glayout/glayout/RL-LLM/enforcer.py
+++ openfasoc/generators/glayout/glayout/RL-LLM/enforcer.py
@@ -4,15 +4,56 @@
 from gdsfactory.pdk import Pdk
 from glayout.flow.pdk.mappedpdk import MappedPDK
 from glayout.flow.pdk.sky130_mapped import sky130_mapped_pdk
+import subprocess
+import os
 
 
+class Enforcer:
+    def __init__(self, pdk: Pdk):
+        self.pdk = pdk
+        self.mapped_pdk = MappedPDK(pdk, sky130_mapped_pdk)
+        self.drc_script = "run_drc.sh"
+        self.drc_output_file = "drc.out"
+        # self.lvs/pex_script = 
+        # self.lvs/pex_output_file =
 
 
-"""
-script_path = "run_drc.sh"
-result = subprocess.run(["bash", script_path], capture_output=True, text=True)
+    
+    def run_drc_check(self, gds_file):
+        if not os.path.exists(self.drc_script):
+            raise FileNotFoundError(f"File {self.drc_script} not found")
+        
+        result = subprocess.run(["bash", self.drc_script, gds_file], capture_output=True, text=True)
 
-print("STDOUT:", result.stdout)
-print("STDERR:", result.stderr)
-"""
+        if result.stderr:
+            raise RuntimeError(f"Error running DRC: {result.stderr}")
+        
+        return self.parse_drc_output()
+
+    def parse_drc_output(self):
+
+        if not os.path.exists(self.drc_output_file):
+            raise FileNotFoundError(f"File {self.drc_output_file} not found")
+        
+        errors = 0
+        with open(self.drc_output_file, "r") as f:
+            for line in f:
+                if "Error" in line:
+                    errors += 1 
+
+        return errors
+    
+
+    # This function is incomplete since i dont know lvs/pex scripts yet for gds file
+    #Also need to find spice netlist generation script from glayout.py or gds
+    def run_lvs_check(self, gds_file, netlist_file):
+        # finish this code
+
+    def reward(self, errors):
+        return 1 / (1 + errors)
+    
+    def enforce(self, gds_file):
+        errors = self.run_drc_check(gds_file)
+        return self.reward(errors)
+
 
--- openfasoc/generators/glayout/glayout/RL-LLM/run_drc.sh
+++ openfasoc/generators/glayout/glayout/RL-LLM/run_drc.sh
@@ -1,3 +1,3 @@
 # run magic
-magic -rcfile $COMMON_VERIF_DIR/sky130A/sky130A.magicrc -noconsole -dnull $COMMON_VERIF_DIR/magic_commands.tcl < /dev/null
+magic -rcfile openfasoc/generators/glayout/glayout/flow/pdk/sky130_mapped/sky130.lydrc/sky130A/sky130A.magicrc -noconsole -dnull magic_commands.tcl < /dev/null
 # -noconsole -dnull < /dev/null
